6f121cf0babe2441299e1467178d7fef8f3d4f9e
vivekmunshiGit <vivekmunshi@outlook.in>
2021-04-07 10:39:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-tayqqs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-tayqqs)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+
+AppComponent(){
+ this.Hide();
+}
+  Hide(){
+    return true;
+  }
+}
